f5cad6acf836f8203bc12efcd1a1b2da80bab6e6
Valentin <vsomoza@fi.uba.ar>
2025-11-06 02:24:42 -0300

    #2: Conecto Flask a la base de datos y escondo las credenciales con dotenv

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,48 @@
 from flask import Flask
+from dotenv import load_dotenv
+from mysql.connector import Error
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
+def iniciarBaseDeDatos():
+    try:
+        conexion = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+        )
+
+        if conexion.is_connected():
+            cursor = conexion.cursor()
+
+            sql_path = os.path.join(os.path.dirname(__file__), "db", "init_db.sql")
+
+            with open(sql_path, "r") as f:
+                sql_script = f.read()
+            
+            for statement in sql_script.split(";"):
+                stmt = statement.strip()
+                if stmt:
+                    cursor.execute(stmt)
+            
+            conexion.commit()
+            print("Base de datos inicializada !")
+    
+    except Error as e:
+        print("Base de datos no pudo inicializarse debido al error ", e)
+    
+    finally:
+        if conexion.is_connected():
+            cursor.close()
+            conexion.close()
+    
+@app.route("/")
+def home():
+    return "Flask se conecto a MariaDB correctamente !"
+
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5001", debug=True)
+
+iniciarBaseDeDatos()
