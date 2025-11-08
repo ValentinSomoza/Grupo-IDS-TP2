d653c397d2c283916ec70cf44442fab8d203e053
Valentin <vsomoza@fi.uba.ar>
2025-11-08 16:04:03 -0300

    #18: Hago un ingreso/registro base

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
+
+app.secret_key = 'texto-que-debe-existir' 
 
 @app.route("/")
 def index():
@@ -20,6 +22,52 @@ def mapa():
 @app.route("/reserva")
 def reserva():
     return render_template('reserva.html')
+
+@app.route("/ingreso", methods=['GET', 'POST'])
+def ingreso():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        contraseña = request.form['contraseña']
+
+        for cliente in clientes:
+            if cliente['usuario'] == usuario and cliente['contraseña'] == contraseña:
+                flash (f' Bienvenido, {cliente["nombre"]} !')
+                return redirect(url_for('index'))
+        
+        flash('Usuario o contraseña incorrectos')
+        return redirect(url_for('ingreso'))
+    return render_template('ingreso.html')
+
+clientes = [] # Lista donde van los clientes temporal
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+
+        nuevo_cliente = { # temporal
+            'nombre': request.form['nombre'],
+            'apellido': request.form['apellido'],
+            'email': request.form['email'],
+            'usuario': request.form['usuario'],
+            'contraseña': request.form['contraseña']
+        }
+
+        for cliente in clientes:
+            if cliente['usuario'] == nuevo_cliente['usuario']:
+                flash('Ese nombre de usuario ya está registrado! Elige otro por favor')
+                return redirect(url_for('registro'))
+            if cliente['email'] == nuevo_cliente['email']:
+                flash('Ese correo electrónico ya está registrado.')
+                return redirect(url_for('registro'))
+
+        clientes.append(nuevo_cliente) # temporal 
+        flash('Usuario registrado con exito !') # temporal
+
+        print(clientes) # verifico si se registro bien
+        # aca se deberian guardar el usuario en la base de datos
+
+        return redirect(url_for('registro'))
+    return render_template('registro.html')
 
 
 if __name__ == "__main__":
