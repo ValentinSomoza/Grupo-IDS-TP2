a0dfe70843f1e80c8b312c424cc70b3986aa73d4
Valentin <vsomoza@fi.uba.ar>
2025-11-12 23:11:01 -0300

    [feature] #20 y #15: Arreglo el flujo del formulario de reserva y lo envio al backend

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
 import requests
 app = Flask(__name__)
 
@@ -23,23 +23,62 @@ def galeria():
 def mapa():
     return render_template('mapa.html')
 
-@app.route("/reserva")
+@app.route('/formularioDatos', methods=['GET', 'POST'])
+def formularioDatos():
+
+    if request.method == 'POST':
+        session['nombre'] = request.form["nombre"]
+        session['apellido'] = request.form["apellido"]
+        session['dniPasaporte'] = request.form["dni-pasaporte"]
+        session['telefono'] = request.form["telefono"]
+        session['email'] = request.form["email"]
+        print("Datos personales recibidos: ", session)
+
+        return redirect(url_for('reserva'))
+
+    return render_template('formularioDatos.html')
+
+@app.route("/reserva", methods=['GET', 'POST'])
 def reserva():
-    return render_template('reserva.html')
 
-@app.route("/reservar", methods=["POST"])
-def reservar():
-    fechaIngreso = request.form["fecha-entrada"]
-    fechaEgreso = request.form["fecha-salida"]
+    if request.method == 'POST':
 
-    datosReserva = {
-        "fechaEntrada": fechaIngreso,
-        "fechaSalida": fechaEgreso
-    }
-    try: 
-        respuesta = requests.post(BACKEND_URL, json=datosReserva)
-    except Exception as e:
-        return f"Error de conexion con el backend: {e}"
+        nombre = session.get('nombre')
+        apellido = session.get("apellido")
+        dniPasaporte = session.get("dniPasaporte")
+        telefono = session.get("telefono")
+        email = session.get("email")
+
+        noches = request.form["noches"]
+        adultos = request.form["adultos"]
+        niños = request.form["niños"]
+        tipoHabitacion = request.form["tipo-habitacion"]
+        numeroHabitacion = request.form["numero-habitacion"]
+        fechaIngreso = request.form["fecha-entrada"]
+        fechaEgreso = request.form["fecha-salida"]
+
+        datosReserva = {
+            "nombre": nombre,
+            "apellido": apellido,
+            "dniPasaporte": dniPasaporte,
+            "telefono": telefono,
+            "email": email,
+            "noches": noches,
+            "adultos": adultos,
+            "niños": niños,
+            "tipoHabitacion": tipoHabitacion,
+            "numeroHabitacion": numeroHabitacion,
+            "fechaEntrada": fechaIngreso,
+            "fechaSalida": fechaEgreso
+        }
+
+        try: 
+            requests.post(BACKEND_URL, json=datosReserva)
+            session.clear()
+        except Exception as e:
+            return f"Error de conexion con el backend: {e}"
+
+    
 
     return render_template('reserva.html')
 
