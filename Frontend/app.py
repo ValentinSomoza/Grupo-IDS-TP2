47decc7a67467ba427399ec9c2f53bf5af87698c
Valentin <vsomoza@fi.uba.ar>
2025-11-29 07:12:44 -0300

    Quito el create_app() para el host

--- Frontend/app.py
+++ Frontend/app.py
@@ -14,54 +14,50 @@ from routes.registro import registro_bp
 from routes.autenticacion import autenticacion_bp
 from dotenv import load_dotenv
 
-def create_app():
+app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
-    app = Flask(__name__)
-    app.secret_key = os.getenv("APP_SECRET_KEY")
+resultadoImagenes = enviarImagenesAlBackend()
+mensajeImagenes = resultadoImagenes.get("mensaje") or resultadoImagenes.get("error")
+print("Frontend: Se enviaron las imagenes al backend y la respuesta fue: ", mensajeImagenes)
 
-    resultadoImagenes = enviarImagenesAlBackend()
-    mensajeImagenes = resultadoImagenes.get("mensaje") or resultadoImagenes.get("error")
-    print("Frontend: Se enviaron las imagenes al backend y la respuesta fue: ", mensajeImagenes)
+resultadoTextos = enviarComentariosYNombresBackend()
+mensajeTextos = resultadoTextos.get("mensaje") or resultadoTextos.get("error")
+numeroNombres = resultadoTextos.get("total_nombres")
+numeroComentarios = resultadoTextos.get("total_comentarios")
+print("Frontend: Se enviaron los textos al backend y la respuesta fue: ", mensajeTextos, " \n Cantidad de nombres cargados: ", numeroNombres, " \n Cantidad de comentarios cargados: ", numeroComentarios)
 
-    resultadoTextos = enviarComentariosYNombresBackend()
-    mensajeTextos = resultadoTextos.get("mensaje") or resultadoTextos.get("error")
-    numeroNombres = resultadoTextos.get("total_nombres")
-    numeroComentarios = resultadoTextos.get("total_comentarios")
-    print("Frontend: Se enviaron los textos al backend y la respuesta fue: ", mensajeTextos, " \n Cantidad de nombres cargados: ", numeroNombres, " \n Cantidad de comentarios cargados: ", numeroComentarios)
+app.register_blueprint(checkin_bp, url_prefix="/checkin")
+app.register_blueprint(cuenta_bp, url_prefix="/cuenta")
+app.register_blueprint(ingreso_bp, url_prefix="/ingreso")
+app.register_blueprint(reservas_bp, url_prefix="/reservas")
+app.register_blueprint(registro_bp, url_prefix="/registro")
+app.register_blueprint(autenticacion_bp, url_prefix="/autenticacion")
 
-    app.register_blueprint(checkin_bp, url_prefix="/checkin")
-    app.register_blueprint(cuenta_bp, url_prefix="/cuenta")
-    app.register_blueprint(ingreso_bp, url_prefix="/ingreso")
-    app.register_blueprint(reservas_bp, url_prefix="/reservas")
-    app.register_blueprint(registro_bp, url_prefix="/registro")
-    app.register_blueprint(autenticacion_bp, url_prefix="/autenticacion")
+@app.route("/")
+def index():
+    return render_template('index.html', backend_url=os.getenv("BACKEND_URL"))
 
-    @app.route("/")
-    def index():
-        return render_template('index.html', backend_url=os.getenv("BACKEND_URL"))
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('404.html'), 404
+@app.route("/galeria")
+def galeria():
+    return render_template("galeria.html", backend_url=os.getenv("BACKEND_URL"))
 
-    @app.route("/galeria")
-    def galeria():
-        return render_template("galeria.html", backend_url=os.getenv("BACKEND_URL"))
-
-    @app.route("/mapa")
-    def mapa():
-        return render_template('mapa.html')
+@app.route("/mapa")
+def mapa():
+    return render_template('mapa.html')
 
 
-    @app.route("/contacto")
-    def contacto():
-        return render_template('contacto.html')
+@app.route("/contacto")
+def contacto():
+    return render_template('contacto.html')
 
-    @app.route("/eligeDatos")
-    def eligeDatos():
-        if estaLogeado() is None:
-            flash("⚠️ Debes iniciar sesión antes de hacer una reserva", "warning")
-            return redirect(url_for('ingreso.ingreso'))
-        return render_template("eligeDatos.html")
-
-    return app
+@app.route("/eligeDatos")
+def eligeDatos():
+    if estaLogeado() is None:
+        flash("⚠️ Debes iniciar sesión antes de hacer una reserva", "warning")
+        return redirect(url_for('ingreso.ingreso'))
+    return render_template("eligeDatos.html")
